3ccaa98be14765b46057e393992953d1a3daea70
X512 <danger_mail@list.ru>
2024-06-20 05:27:14 +0900

    fix Haiku build

--- src/common/inc/nvVer.h
+++ src/common/inc/nvVer.h
@@ -6,7 +6,7 @@
 #define NV_COPYRIGHT_YEAR               "2024"
 #define NV_COPYRIGHT                    "(C) " NV_COPYRIGHT_YEAR " NVIDIA Corporation. All rights reserved."  // Please do not use the non-ascii copyright symbol for (C).
 
-#if defined(NV_LINUX) || defined(NV_BSD) || defined(NV_SUNOS) || defined(NV_VMWARE) || defined(NV_QNX) || defined(NV_INTEGRITY) || \
+#if defined(NV_LINUX) || defined(NV_BSD) || defined(NV_SUNOS) || defined(NV_VMWARE) || defined(NV_QNX) || defined(NV_INTEGRITY) || defined(NV_UNIX) || \
     (defined(RMCFG_FEATURE_PLATFORM_GSP) && RMCFG_FEATURE_PLATFORM_GSP == 1)
 
 // All Version numbering for Unix builds has moved.  (Source should be re-directed to directly include that header.)
